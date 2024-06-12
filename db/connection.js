b1cb09c9b9b1dcc91d77248df7d45e8e9e7625b2
Gnana Prashmitha Gullipalli <112489044+prashmitha03@users.noreply.github.com>
2024-06-12 16:00:02 +0530

    Add files via upload

---
+++ db/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',      
+    user: 'root',           
+    password: '',           
+    database: 'expense'     
+});
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.stack);
+        return;
+    }
+    console.log('Connected to database');
+});
+module.exports = connection;
