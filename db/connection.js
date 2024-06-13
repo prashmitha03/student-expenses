03b327fac5f0a59ad446c49b3d27e25d181d42d2
Gnana Prashmitha Gullipalli <112489044+prashmitha03@users.noreply.github.com>
2024-06-13 12:53:14 +0530

    Add files via upload

---
+++ db/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err.stack);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+module.exports = connection;
